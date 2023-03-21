cd27947e9616da2bc47da2ba428620afdff5edbf
pedroripper <pedros.ripper@gmail.com>
2023-03-21 16:07:24 -0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -27,6 +27,7 @@ docs = [
     ]),
 ]
 
+
 outpath = mktempdir()
 
 MultiDocumenter.make(
@@ -34,6 +35,38 @@ MultiDocumenter.make(
     docs;
     search_engine = MultiDocumenter.SearchConfig(
         index_versions = ["stable"],
-        engine = MultiDocumenter.FlexSearch
-    )
+        engine = MultiDocumenter.FlexSearch,
+    ),
+    rootpath = "/MultiDocumenter.jl/",
 )
+
+gitroot = normpath(joinpath(@__DIR__, ".."))
+run(`git pull`)
+outbranch = "gh-pages"
+has_outbranch = true
+if !success(`git checkout $outbranch`)
+    has_outbranch = false
+    if !success(`git switch --orphan $outbranch`)
+        @error "Cannot create new orphaned branch $outbranch."
+        exit(1)
+    end
+end
+for file in readdir(gitroot; join = true)
+    endswith(file, ".git") && continue
+    rm(file; force = true, recursive = true)
+end
+for file in readdir(outpath)
+    cp(joinpath(outpath, file), joinpath(gitroot, file))
+end
+run(`git add .`)
+if success(`git commit -m 'Aggregate documentation'`)
+    @info "Pushing updated documentation."
+    if has_outbranch
+        run(`git push`)
+    else
+        run(`git push -u origin $outbranch`)
+    end
+    run(`git checkout main`)
+else
+    @info "No changes to aggregated documentation."
+end
