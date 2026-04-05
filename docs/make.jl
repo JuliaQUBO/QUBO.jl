6b018f5e14d7e2aa50ec4a001dbec22df641726f
David E. Bernal Neira <dbernaln@purdue.edu>
2026-04-05 02:11:33 -0400

    Fix docs metadata and vendored logos

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,7 @@ makedocs(;
         sidebar_sitename = false
     ),
     sitename = "QUBO.jl",
-    authors  = "Pedro Maciel Xavier and Pedro Ripper and Tiago Andrade and Joaquim Dias Garcia and David E. Bernal Neira",
+    authors  = "Pedro Maciel Xavier and Pedro Ripper and Tiago Andrade and Joaquim Dias Garcia and Nelson Maculan and David E. Bernal Neira",
     pages    = [
         "Home" => "index.md",
         "Examples" => [
