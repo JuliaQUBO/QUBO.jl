17845021c15aeb1da85891b0c4cccd19024a9a76
David E. Bernal Neira <dbernaln@purdue.edu>
2026-05-23 15:02:08 -0400

    Fix Julia CI floor modernization (#20)

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,11 @@ using QUBO
 include("spin_model.jl")
 include("multimake_utils.jl")
 include("docs_assets.jl")
+include("package_metadata.jl")
 
 function main()
     test_docs_assets()
+    test_package_metadata()
     test_spin_model()
     test_multimake_utils()
 
