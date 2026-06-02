0b8dcde332cf665e4dfa1f1b2e424c5e1dd539da
David E. Bernal Neira <dbernaln@purdue.edu>
2026-06-01 20:15:39 -0400

    docs: document dependency maintenance policy (#30)

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,7 @@ makedocs(;
     pages    = [
         "Home" => "index.md",
         "Design & Architecture" => "design.md",
+        "Dependency Maintenance" => "maintenance.md",
         "Analysis & Visualization" => "analysis.md",
         "Examples" => [
             "Knapsack" => "examples/knapsack.md",
